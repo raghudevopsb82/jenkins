708f5598cfa1541baca9193983260958b2c2a480
SVR Kalluru <raghuk.vit@gmail.com>
2025-02-15 12:11:24 +0530

    RoboShop

---
+++ shared-library/vars/ci.groovy
@@ -0,0 +1,42 @@
+def call() {
+  pipeline {
+    agent any
+
+    stages {
+
+      stage('app-dependencies') {
+        steps {
+          sh 'echo OK'
+        }
+      }
+
+      stage('static-code-analysis') {
+        steps {
+          sh 'echo OK'
+        }
+      }
+
+      stage('unit-tests') {
+        steps {
+          sh 'echo OK'
+        }
+      }
+
+      stage('build-container-image') {
+        steps {
+          sh 'echo OK'
+        }
+      }
+
+      stage('push-to-image-registry') {
+        steps {
+          sh 'echo OK'
+        }
+      }
+
+    }
+  }
+
+
+
+}
